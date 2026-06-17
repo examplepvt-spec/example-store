a401cd0fc2b6e9ec31f6273ecfa4fdff7698cdd2
examplepvt-spec <examplepvt@gmail.com>
2026-06-17 14:43:59 +0530

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,1 +1,15 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "example-clothing.firebaseapp.com",
+  projectId: "example-clothing",
+  storageBucket: "example-clothing.firebasestorage.app",
+  messagingSenderId: "365541402622",
+  appId: "1:365541402622:web:129d716d298bb2de9342af"
+};
+
+const app = initializeApp(firebaseConfig);
+
 console.log("Firebase Connected");
